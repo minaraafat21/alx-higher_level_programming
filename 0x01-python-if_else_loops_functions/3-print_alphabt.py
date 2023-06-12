0298943030692c19434b4fa534f23f00772bb91a
mina <minaraafatemil14@gmail.com>
2023-06-12 08:52:54 +0300

    MON

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter != 133 or letter != 101:
+    if letter != 133 and letter != 101:
         print("{}".format(chr(letter)), end="")
-        
