849247967578461aa83ceabfa8a2c5674551ec42
mina <minaraafatemil14@gmail.com>
2023-06-13 18:35:47 +0300

    MON

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    Mymax = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > Mymax:
+            Mymax = my_list[i]
+    if not my_list:
+        return None
+    return Mymax
