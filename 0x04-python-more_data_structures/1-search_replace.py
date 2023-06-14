aef6de31994923727ecf2a25b421773794664952
mina <minaraafatemil14@gmail.com>
2023-06-14 21:01:15 +0300

    MON

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in range (len(my_list)):
+        if my_list[i] == search:
+            new_list = my_list[:i] + [replace] + my_list[i + 1:]
+    return new_list
