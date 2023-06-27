277572853568935db839d16751e09f0d3d2ed9c4
mina <minaraafatemil14@gmail.com>
2023-06-27 08:19:41 +0300

    MON

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """size validation"""
+
+    def __init__(self, size=0):
+        """ init constructor"""
+
+        self.__size = size
+
+    def area(self):
+        """return area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
