ad68bfd967b58659d86c3daf97118b35136bdbe4
mina <minaraafatemil14@gmail.com>
2023-06-27 07:36:13 +0300

    MON

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """size validation"""
+    def __init__(self, size=0):
+        """ init constructor"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """return area"""
+        return self.__size ** 2
