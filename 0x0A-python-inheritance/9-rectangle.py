a524a6f377f89ffd87483e4435d3da3313de884d
mina <minaraafatemil14@gmail.com>
2023-07-10 20:23:58 +0300

    mon

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -20,3 +20,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Returns the rectangle description"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
