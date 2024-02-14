89246b349a025fe5fa3ea6747ab19f5eab8638f7
mina <minaraafatemil14@gmail.com>
2024-02-14 16:53:46 +0200

    MON

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,7 @@
-3
-rts.callMeMoby = function (x, theFunction) {
+function callMeMoby (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
-};
+}
+
+module.exports.callMeMoby = callMeMoby;
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-    console.log('Missing size');
-  } else {
-    const x = Number(process.argv[2]);
-    let i = 0;
-    let j = 0;
-    while (i < x) {
-        j = 0;
-        while (j < x) {
-            process.stdout.write('X');
-            j++;
-        }
-        console.log();
-      i++;
+  console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  let j = 0;
+  while (i < x) {
+    j = 0;
+    while (j < x) {
+      process.stdout.write('X');
+      j++;
     }
+    console.log();
+    i++;
   }
+}
