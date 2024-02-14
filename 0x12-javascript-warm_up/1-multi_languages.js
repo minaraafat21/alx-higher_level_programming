a9334b88e6b4957885b55c2c87def7ee38d4d63f
mina <minaraafatemil14@gmail.com>
2024-02-14 16:57:34 +0200

    MON

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 const myVar = 'Javascript is amazing';
 const myVar2 = 'C is fun';
 const myVar3 = 'Python is cool';
-console.log(myVar);
 console.log(myVar2);
 console.log(myVar3);
+console.log(myVar);
