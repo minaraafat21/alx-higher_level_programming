c72691b81a607c15f0497cd9a4d2e68fe15d0933
mina <minaraafatemil14@gmail.com>
2024-02-18 22:04:47 +0200

    MON

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,10 +8,12 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1].startswith("N"):
-            print(row)
+        print(row)
     cur.close()
     conn.close()
