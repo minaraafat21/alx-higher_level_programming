3333721351e501c85f163933cba45631d7b071f3
mina <minaraafatemil14@gmail.com>
2024-02-18 21:17:15 +0200

    first

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    # Connect to the MySQL database
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+
+    # Execute the SQL query to select all the states
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    
+    # Fetch all the rows returned by the query
+    query_rows = cur.fetchall()
+
+    # Print each row
+    for row in query_rows:
+     if row[1].startswith("N"):
+        print(row)
+
+    # Close the cursor and the database connection
+    cur.close()
+    conn.close()
