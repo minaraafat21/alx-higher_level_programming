e36f50ffd83328bbf85aac63b8cae1f1ee3b89b2
mina <minaraafatemil14@gmail.com>
2024-02-18 22:14:54 +0200

    mon

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+"""Lists states"""
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    try:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                               passwd=argv[2], db=argv[3], charset="utf8")
-        cur = conn.cursor()
-
-        # Use a parameterized query
-        sql = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC"
-        cur.execute(sql, (argv[4],))
-
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-
-    finally:
-        cur.close()
-        conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
