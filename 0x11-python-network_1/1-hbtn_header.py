61696344a2e930d77ebec9ae46a375e15f4e1dbb
mina <minaraafatemil14@gmail.com>
2024-03-02 00:49:00 +0200

    MON

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     req = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as res:
+    with urllib.request.urlopen(req) as res:
         print(dict(res.headers).get("X-Request-Id"))
