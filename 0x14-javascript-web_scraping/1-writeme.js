81133be9263937b845bf57222d9402dcb19df91f
mina <minaraafatemil14@gmail.com>
2024-03-27 01:37:14 +0200

    MON

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,9 +2,8 @@
 const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(file, content, 'utf8', (err) => {
+fs.writeFile(file, content, 'utf8', function (err) {
   if (err) {
     console.log(err);
-    return;
   }
 });
