731a4d364029d197f08582e70dd842910c5bce24
mina <minaraafatemil14@gmail.com>
2024-03-27 01:53:07 +0200

    MON

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const my_url = process.argv[2];
-request(my_url, function (err, response) {
+const url = process.argv[2];
+request(url, function (err, response) {
   if (err) {
     console.log(err);
   } else {
