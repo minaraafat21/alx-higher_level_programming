f531c8b0671c936cdf302ca6629c77baa64fbc3d
mina <minaraafatemil14@gmail.com>
2024-03-27 01:06:00 +0200

    MON

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require("fs"); // CommonJS syntax
 my_file = process.argv[2];
 fs.readFile(my_file, "utf8", function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
   console.log(data);
