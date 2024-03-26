e6394ac0229c10de7097f57202a551287e2f2c58
mina <minaraafatemil14@gmail.com>
2024-03-27 01:10:57 +0200

    MON

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs');
-const my_file = process.argv[2];
-fs.readFile(my_file, 'utf-8', function (err, data) {
+const fs = require("fs");
+const file = process.argv[2];
+fs.readFile(file, "utf8", function (err, data) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
+    return;
   }
+  console.log(data);
 });
